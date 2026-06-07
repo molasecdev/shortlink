8d0af7c469f18f1e4d11b8172555d233f188590d
molasecdev <mola.secdev@gmail.com>
2026-06-07 11:39:49 +0700

    move constants.ts to root folder and install zod

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import vercel from '@astrojs/vercel';
+import { info } from './constants';
 
 const isVercel = Boolean(process.env.VERCEL);
 
 export default defineConfig({
+  site: isVercel ? info.siteUrl : info.localSiteUrl,
   integrations: [tailwind()],
   output: 'server',
   adapter: isVercel
--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,4 +1,4 @@
-import { defaultConfig, info } from "@/constants";
+import { defaultConfig, info } from "../../constants";
 import fs from "fs/promises";
 import path from "path";
 
